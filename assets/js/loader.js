d5408ecaa5b06d788aa55b25daae53e70a005e11
ks-Rafalimanana <sebastien@kasia.mg>
2022-10-01 13:06:46 +0200

    lading + teams

---
+++ assets/js/loader.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", async(e) => {
+    let loader = document.querySelector('.loader')
+    setTimeout(() => {
+        loader.style.display = "none"
+        return true
+    }, 5000)
+})
